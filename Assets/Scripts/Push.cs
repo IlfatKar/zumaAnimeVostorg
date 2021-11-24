965d433e362a3b08f0e7874e5e05261dd11c86c3
kwinc <pkv.ko@yandex.ru>
2021-11-24 16:26:22 +0300

    Pipec

--- Assets/Scripts/Push.cs
+++ Assets/Scripts/Push.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Push : MonoBehaviour
 {
+    public float ImpSpeed=5;
+    public GameObject BallPrefab;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Debug.Log("Click!");
             //что-то умное чтобы стреляло            
-            //GetComponent<Rigidbody2D>().AddForce(new Vector2(ImpSpeed,0));
+            //Instantiate(BallPrefab, new Vector3(transform.position.x, transform.position.y, -1), Quaternion.identity);
+            //GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 1) * ImpSpeed, ForceMode2D.Impulse);
         }
     }
 }
